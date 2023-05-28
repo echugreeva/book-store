74a4d7a62223dadb3ce8768831714049025a33d0
echugreeva <e.chugreeva@gmail.com>
2023-05-28 20:36:07 +0300

    quantity logic started

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -5,12 +5,18 @@ import { useNavigate } from 'react-router-dom'
 const Cart = () => {
     const {book, setBook, cart, setCart}=useContext(AppContext)
     const [sum, setSum] = useState(0)
+    const [localCart, setLocal] = useState( [...new Set(cart)])
+    const [quantity,setQuantity] = useState([])
     const navigate =useNavigate()
-
+    
     useEffect(()=>{
         const total=cart.reduce((acc,curr)=>acc + curr.price,0)
         setSum(total)
+        const bookquantity = localCart.map(value => [value.id, cart.filter(str => str === value).length]);
+        setQuantity(bookquantity)
     },[cart])
+
+   
 
     if (cart.length < 1) {
         return (
@@ -24,12 +30,16 @@ const Cart = () => {
     return (
         <div>
             {
-                cart.map(book=>{
+                localCart.map(book=>{
+                   console.log(quantity)
                     // 
                     return(
                         <div key={book.id}>
                             {book.name}
                             <p>price: ${book.price}</p>
+                            <p>quantity: {quantity.filter(e=>e[0] === book.id
+            
+                            )}</p>
                             <button
                                 onClick={()=> {
                                         setCart(cart.filter(e=>e.id !== book.id))
